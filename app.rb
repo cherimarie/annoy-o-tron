db5df223bdd58d764de2231f9550f00787bb08b3
Cheri Allen <cherimarie@gmail.com>
2019-09-22 22:06:59 -0700

    Use keys to limit who can bother robert

--- app.rb
+++ app.rb
@@ -4,20 +4,24 @@ require 'yaml'
 
 secrets = YAML.load_file('secrets.yml')
 
-get '/bother-robert/:smartass' do
+get '/bother-robert' do
 
-  print("Hello #{params['smartass']}!")
-  account_sid = secrets["twilio_sid"]
-  auth_token = secrets["twilio_token"]
-  client = Twilio::REST::Client.new(account_sid, auth_token)
+  if secrets["auth_keys"].include?(params['key'])
+    account_sid = secrets["twilio_sid"]
+    auth_token = secrets["twilio_token"]
+    client = Twilio::REST::Client.new(account_sid, auth_token)
 
-  from = secrets["from_number"]
-  to = secrets["robert_number"]
+    from = secrets["from_number"]
+    to = secrets["robert_number"]
 
-  client.messages.create(
-    from: from,
-    to: to,
-    body: "#{params['smartass']}"
-  )
-  return "looool"
+    client.messages.create(
+      from: from,
+      to: to,
+      body: "#{params['smartass']}"
+    )
+    return " 🤣  #{params['smartass']}!"
+  else
+    return "unauthorized, sucker"
+  end
+
 end
