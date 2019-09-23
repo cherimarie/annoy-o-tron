a4262d8e9857ac445dc21bb72bdf710cf508c48f
Cheri Allen <cherimarie@gmail.com>
2019-09-22 21:57:48 -0700

    Can text robert annyoing things

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'twilio-ruby'
+require 'yaml'
+
+secrets = YAML.load_file('secrets.yml')
+
+get '/bother-robert/:smartass' do
+
+  print("Hello #{params['smartass']}!")
+  account_sid = secrets["twilio_sid"]
+  auth_token = secrets["twilio_token"]
+  client = Twilio::REST::Client.new(account_sid, auth_token)
+
+  from = secrets["from_number"]
+  to = secrets["robert_number"]
+
+  client.messages.create(
+    from: from,
+    to: to,
+    body: "#{params['smartass']}"
+  )
+  return "looool"
+end
